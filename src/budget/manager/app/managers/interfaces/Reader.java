0497c90204ee2bd888ef1aee0077e3a0055c42da
Angel <stu2201681028@uni-plovdiv.bg>
2024-12-19 15:16:34 +0200

    Removing csv servises from managers

--- src/budget/manager/app/managers/interfaces/Reader.java
+++
@@ -1,8 +0,0 @@
-package budget.manager.app.managers.interfaces;
-
-import java.io.Serializable;
-import java.util.List;
-
-public interface Reader {
-     <T extends Serializable> List<T> read(String filePath, String fileName);
-}
--- src/budget/manager/app/managers/interfaces/Writer.java
+++
@@ -1,8 +0,0 @@
-package budget.manager.app.managers.interfaces;
-
-import java.io.Serializable;
-import java.util.List;
-
-public interface Writer {
-    void write(List<? extends Serializable> list, String fileName);
-}
